4f41068d3a299e3a52c3f01eb0d2edfebec966ec
EMEBE <81788252+MilthonB@users.noreply.github.com>
2025-10-13 20:02:21 -0600

    archivo env.dart agregado

---
+++ lib/src/config/helper/env.dart
@@ -0,0 +1,9 @@
+
+
+class Env {
+  Env._();
+  static String get baseUrlDev => 'https://dummyjson.com';
+  static String get baseUrlStaging => 'https://dummyjson.com';
+  static String get baseUrlProd => 'https://dummyjson.com';
+  static String get baseUrl => 'https://dummyjson.com';
+}
